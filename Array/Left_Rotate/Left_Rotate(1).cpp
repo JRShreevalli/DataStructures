7ba93f97324082ec4d0a3625bca27beef8d44eee
JRShreevalli <70849567+JRShreevalli@users.noreply.github.com>
2021-12-09 12:12:38 +0530

    Create Left_Rotate(1).cpp

---
+++ Array/Left_Rotate/Left_Rotate(1).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n , i ;
+    cin >> n ;
+    int a[n] ;
+    for (i=0 ; i<n ; i++)
+        cin >> a[i] ;
+
+    int temp1 = a[0] ;
+
+    for (i=0 ; i<n ; i++)
+    {
+        if (i == n-1)
+            a[i] = temp1 ;
+        else
+            a[i] = a[i+1] ;
+    }
+    for (i=0 ; i<n ; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0 ;
+}
