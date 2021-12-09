93e83b0c4a407d2758653dfe4866bddc1fcef558
JRShreevalli <70849567+JRShreevalli@users.noreply.github.com>
2021-12-09 12:13:13 +0530

    Add files via upload

---
+++ Array/Left_Rotate/LeftRotate_by_D_times.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std ;
+
+int Left_Rot(int arr[] , int n)
+{
+    int i ;
+    int temp1 = arr[0] ;
+    for (i=0 ; i<n ; i++)
+    {
+        if (i == n-1)
+            arr[i] = temp1 ;
+        else
+            arr[i] = arr[i+1] ;
+    }
+}
+int Left_Rototate_D_times(int a[], int n, int d)
+{
+    int i ;
+    for(i=0 ; i<d ; i++)
+        Left_Rot(a,n);
+    return 0 ;
+}
+int main()
+{
+    int n , i ;
+    cin >> n ;
+    int a[n] , d ;
+    cin >> d ;
+    for (i=0 ; i<n ; i++)
+        cin >> a[i] ;
+    Left_Rototate_D_times(a, n , d);
+    for( i=0 ; i<n ; i++)
+        cout << a[i] <<" " ;
+return 0 ;
+}
