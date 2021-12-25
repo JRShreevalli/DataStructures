fbdb737abdc9114dcca2e4cf261fd6cb84a5a723
JRShreevalli <70849567+JRShreevalli@users.noreply.github.com>
2021-12-25 07:55:07 +0530

    Add files via upload

---
+++ Searching/Index of first Occurence/_2_Index_Of_1st_occurence_Recursive.cpp
@@ -0,0 +1,41 @@
+// The elements in array are already sorted !!!!!!!!
+// REcurssion 
+#include <iostream>
+using namespace std ;
+
+int Search(int arr[] , int low , int high , int  ele)
+{
+    while(low <= high)
+    {
+        int mid = (low + high ) / 2 ;
+
+        if(arr[mid] == ele)
+                return mid ;
+
+        else if (arr[mid] < ele)
+                return Search(arr,mid+1,high,ele);
+
+        else if (arr[mid] > ele)
+                return Search(arr,low,mid+1,ele);
+        else
+        {
+            if(mid == 0 || arr[mid] != arr[mid-1])
+                    return mid ;
+            else
+                    return Search(arr,low,mid-1,ele) ;
+        }
+    }
+}
+
+int main()
+{
+    int n , i , x ;
+    cin >> n ;
+    int a[n] ;
+    for (i=0 ; i<n ; i++)
+        cin >> a[i] ;
+    cout << "Search element :" << " " ;
+    cin >> x ;
+    cout <<  Search(a,0,n-1,x) ; 
+return 0 ;
+}
