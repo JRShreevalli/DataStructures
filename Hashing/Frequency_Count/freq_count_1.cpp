9acea64c05a322465d6d1a1f5c5f89b334927c04
J Shreevalli <70849567+JRShreevalli@users.noreply.github.com>
2022-01-14 18:10:49 +0530

    Create freq_count_1.cpp

---
+++ Hashing/Frequency_Count/freq_count_1.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void countFreq(int arr[], int n)
+{
+    // Mark all array elements as not visited
+    vector <bool> visited(n, false);
+ 
+    // Traverse through array elements and
+    // count frequencies
+    for (int i = 0; i < n; i++) 
+    {
+ 
+        // Skip this element if already processed
+        if (visited[i] == true)
+            continue;
+ 
+        // Count frequency
+        int count = 1;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j]) 
+            {
+                visited[j] = true;
+                count++;
+            }
+        }
+        cout << arr[i] << " " << count << endl;
+    }
+}
+ 
+int main()
+{
+    
+    int t, i ;
+    cin >> t ;
+    int a[t] ;
+    for(i=0 ; i<t ; i++)
+        cin >> a[i] ;
+    countFreq(a,t) ;
+    return 0;
+}
